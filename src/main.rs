06dc759a9b105f3fe83f018ca6615b518c75bfdb
Chris O'Brian <mobiletread@gmail.com>
2023-04-26 20:56:39 -0700

    playing with bootstrap

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,38 @@ async fn main() {
                 Html(format!(
                     r#"
                 <!DOCTYPE html>
-                <html>
-                <head> <title>Dioxus LiveView with Axum</title>  </head>
-                <body> <div id="main"></div> </body>
+                <html data-bs-theme="dark" lang="en">
+                <head> 
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+                    <title>Dioxus LiveView with Axum</title>  
+                </head>
+                <body> 
+                    <div class="container my-5">
+                    
+  <div class="p-5 text-center bg-body-tertiary rounded-3">
+    <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100"><use xlink:href='#bootstrap'></use></svg>
+    <h1 class="text-body-emphasis">Jumbotron with icon</h1>
+    <p class="col-lg-8 mx-auto fs-5 text-muted">
+      This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive <code>.col-*</code> class, and a customized call to action.
+    </p>
+    <div class="d-inline-flex gap-2 mb-5">
+      <button class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill" type="button">
+        Call to action
+        <svg class="bi ms-2" width="24" height="24"><use xlink:href='#arrow-right-short'></use></svg>
+      </button>
+      <button class="btn btn-outline-secondary btn-lg px-4 rounded-pill" type="button">
+        Secondary link
+      </button>
+    </div>
+  </div>
+
+                    </div> 
+                <div id="main" class="container my-5">
                 {glue}
+                </div>
+                </body>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
                 </html>
                 "#,
                     // Create the glue code to connect to the WebSocket on the "/ws" route
@@ -56,6 +84,9 @@ fn app(cx: Scope) -> Element {
     })
 }
 
+//
+// Meme Editor project
+//
 #[inline_props]
 fn CaptionEditor<'a>(
     cx: Scope<'a>,
@@ -143,3 +174,4 @@ fn MemeEditor(cx: Scope) -> Element {
         }
     })
 }
+//
